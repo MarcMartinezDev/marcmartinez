334b5820507dca15ec52222cf1687ea607697d14
marcmartinezdev <marcmh1605@gmail.com>
2025-06-04 08:32:00 +0200

    init

---
+++ src/config.ts
@@ -0,0 +1,4 @@
+export const SITE_URL = import.meta.env.PUBLIC_SITE_URL;
+export const DEFAULT_LOCALE = import.meta.env.PUBLIC_I18N_DEFAULT_LOCALE;
+export const STRAPI_API_URL = import.meta.env.STRAPI_API_URL;
+export const STRAPI_API_TOKEN = import.meta.env.STRAPI_API_TOKEN;
