87af9b25312107bc84f7328484edc82de49274c9
marc martinez <marcmh1605@gmail.com>
2025-06-12 13:29:11 +0200

    add IT language

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   i18n: {
       defaultLocale: "es",
-      locales: ["en", "es"]
+      locales: ["en", "es", "it"]
   },
 
   vite: {
