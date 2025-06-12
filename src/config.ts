82111389eef58a513789f51d57663fa3206fe2da
marc martinez <marcmh1605@gmail.com>
2025-06-12 11:01:08 +0200

    all: refactor code

--- src/config.ts
+++
@@ -1,8 +0,0 @@
-export const SITE_URL =
-  import.meta.env.PUBLIC_SITE_URL || "http://localhost:3000";
-export const DEFAULT_LOCALE =
-  import.meta.env.PUBLIC_I18N_DEFAULT_LOCALE || "es";
-export const SUPPORTED_LOCALES = import.meta.env
-  .PUBLIC_I18N_SUPPORTED_LOCALES || ["en", "es"];
-export const STRAPI_API_URL = import.meta.env.STRAPI_API_URL;
-export const STRAPI_API_TOKEN = import.meta.env.STRAPI_API_TOKEN;
