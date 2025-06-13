cf8d9481b5b631ae2ef5716e11fc6695c69e06de
marc martinez <marcmh1605@gmail.com>
2025-06-13 13:22:12 +0200

    add metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,29 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "http://localhost:4321",
+  integrations: [
+    sitemap({
+      i18n: {
+        defaultLocale: "es",
+        locales: {
+          en: "en-US",
+          es: "es-ES",
+          it: "it-IT",
+        },
+      },
+    }),
+  ],
   i18n: {
-      defaultLocale: "es",
-      locales: ["en", "es", "it"]
+    defaultLocale: "es",
+    locales: ["en", "es", "it"],
   },
 
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
